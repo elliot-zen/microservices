5bae21d0d64038c4901e70463c5399b6b0851195
elliot <elliotzen256@gmail.com>
2025-03-23 18:50:55 +0800

    layer-port&application

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    allSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+      "x86_64-darwin" # 64-bit Intel macOS
+      "aarch64-darwin" # 64-bit ARM macOS
+    ];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        name = "Go";
+        packages = with pkgs; [
+          go
+          gopls
+          wire
+          gotools
+        ];
+      };
+    });
+  };
+}
---
+++ order/go.mod
@@ -0,0 +1,3 @@
+module github.com/elliot-zen/microservices/order
+
+go 1.23.6
